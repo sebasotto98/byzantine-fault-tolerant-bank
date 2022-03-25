754803e2f940e87c9078a0cd9f16f8e91c0dd8ed
g2pinto <gustavo.pinto@tecnico.ulisboa.pt>
2022-03-25 14:27:30 +0000

    create aux function with socket impl

--- src/main/java/pt/tecnico/API.java
+++ src/main/java/pt/tecnico/API.java
@@ -32,98 +32,9 @@ public class API {
                             int serverPort, InetAddress serverAddress, PublicKey bankPublic) 
                             throws GeneralSecurityException, IOException  {
 
-        // Timestamps are in UTC
-		Instant inst = Instant.now().plus(SOCKET_TIMEOUT, ChronoUnit.MINUTES);
+        String body = sendMessageAndReceiveBody(accountPublicKey, accountPrivateKey, clientPort, serverPort, serverAddress, bankPublic, "OpenAccount");
 		
-		//final String SYM_ALGO = "AES/CBC/PKCS5Padding";
-
-		MessageDigest msgDig = MessageDigest.getInstance(DIGEST_ALGO);
-
-		//KeyPair keys = read("keys/pis_public_key.der","keys/pis_private_key.der");
-
-		//PublicKey bankPublic = readPublic("keys/bank_public_key.der");
-		Cipher encryptCipher = Cipher.getInstance(CIPHER_ALGO);
-		//encryptCipher.init(Cipher.ENCRYPT_MODE, bankPublic);
-
-		//Cipher symCipher = Cipher.getInstance(SYM_ALGO);
-		//SecretKey symKey = generateSessionKey();
-		//IvParameterSpec iv = generateIv();
-		//symCipher.init(Cipher.ENCRYPT_MODE, symKey, iv);
-
-		Cipher signCipher = Cipher.getInstance(CIPHER_ALGO);
-		signCipher.init(Cipher.ENCRYPT_MODE, accountPrivateKey);
-
-		// Concat IV and session key to send
-		//ByteArrayOutputStream outputStream = new ByteArrayOutputStream(symKey.getEncoded().length + iv.getIV().length);
-		//outputStream.write(symKey.getEncoded());
-		//outputStream.write(iv.getIV());
-
-		// Create socket
-		DatagramSocket socket = new DatagramSocket(clientPort);
-        // Create request message
-		JsonObject requestJson = JsonParser.parseString("{}").getAsJsonObject();
-		
-        JsonObject infoJson = JsonParser.parseString("{}").getAsJsonObject();
-        infoJson.addProperty("to", "BFTB");
-		infoJson.addProperty("from", accountPublicKey.getEncoded().toString());
-
-        String bodyText = "OpenAccount";
-        //byte[] cipheredBody = symCipher.doFinal(bodyText.getBytes());
-        //String bodyEnc = Base64.getEncoder().encodeToString(cipheredBody);
-        infoJson.addProperty("body", bodyText);
-        infoJson.addProperty("instant", inst.toString());
-
-        requestJson.add("info", infoJson);
-
-        msgDig.update(infoJson.toString().getBytes());
-        String macString = Base64.getEncoder().encodeToString(signCipher.doFinal(msgDig.digest()));
-        requestJson.addProperty("MAC", macString);
-		
-		System.out.println("Request message: " + requestJson);
-		
-		// Send request
-		byte[] clientData = requestJson.toString().getBytes();
-		System.out.printf("%d bytes %n", clientData.length);
-		DatagramPacket clientPacket = new DatagramPacket(clientData, clientData.length, serverAddress, serverPort);
-		socket.send(clientPacket);
-		System.out.printf("Request packet sent to %s:%d!%n", serverAddress, serverPort);
-
-		// Receive response
-		byte[] serverData = new byte[BUFFER_SIZE];
-		DatagramPacket serverPacket = new DatagramPacket(serverData, serverData.length);
-		System.out.println("Wait for response packet...");
-		socket.receive(serverPacket);
-		System.out.printf("Received packet from %s:%d!%n", serverPacket.getAddress(), serverPacket.getPort());
-		System.out.printf("%d bytes %n", serverPacket.getLength());
-
-		inst = Instant.now();
-		//symCipher.init(Cipher.DECRYPT_MODE, symKey, iv);
-		encryptCipher.init(Cipher.DECRYPT_MODE, bankPublic);
-
-		// Convert response to string
-		String serverText = new String(serverPacket.getData(), 0, serverPacket.getLength());
-		System.out.println("Received response: " + serverText);
-
-		// Parse JSON and extract arguments
-		JsonObject responseJson = JsonParser.parseString(serverText).getAsJsonObject();
-        JsonObject infoBankJson;
-		String from, body, to, mac, instantBank;
-		
-        infoBankJson = responseJson.getAsJsonObject("info");
-        from = infoBankJson.get("from").getAsString();
-        to = infoBankJson.get("to").getAsString();
-        body = infoBankJson.get("body").getAsString();
-        instantBank = infoBankJson.get("instant").getAsString();
-        
-        mac = responseJson.get("MAC").getAsString();
-		
-        int messageCheck = checkMessage(encryptCipher, mac, msgDig, infoBankJson, instantBank, inst);
-		//String bodyDec = new String(symCipher.doFinal(Base64.getDecoder().decode(body)));
-		// Close socket
-		socket.close();
-		System.out.println("Socket closed");
-		
-		if (messageCheck == CORRECT && body.equals("AccountCreated")) {
+		if (body.equals("AccountCreated")) {
 
 			writeToCSV("csv_files/clients.csv", new String[]{accountPublicKey.getEncoded().toString(), "1000", "1000"});
 
@@ -193,6 +104,108 @@ public class API {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private String sendMessageAndReceiveBody(PublicKey accountPublicKey, PrivateKey accountPrivateKey, int clientPort,
+											int serverPort, InetAddress serverAddress, PublicKey bankPublic, String bodyText) 
+											throws GeneralSecurityException, IOException  {
+		
+		// Timestamps are in UTC
+		Instant inst = Instant.now().plus(SOCKET_TIMEOUT, ChronoUnit.MINUTES);
+		
+		//final String SYM_ALGO = "AES/CBC/PKCS5Padding";
+
+		MessageDigest msgDig = MessageDigest.getInstance(DIGEST_ALGO);
+
+		//KeyPair keys = read("keys/pis_public_key.der","keys/pis_private_key.der");
+
+		//PublicKey bankPublic = readPublic("keys/bank_public_key.der");
+		Cipher encryptCipher = Cipher.getInstance(CIPHER_ALGO);
+		//encryptCipher.init(Cipher.ENCRYPT_MODE, bankPublic);
+
+		//Cipher symCipher = Cipher.getInstance(SYM_ALGO);
+		//SecretKey symKey = generateSessionKey();
+		//IvParameterSpec iv = generateIv();
+		//symCipher.init(Cipher.ENCRYPT_MODE, symKey, iv);
+
+		Cipher signCipher = Cipher.getInstance(CIPHER_ALGO);
+		signCipher.init(Cipher.ENCRYPT_MODE, accountPrivateKey);
+
+		// Concat IV and session key to send
+		//ByteArrayOutputStream outputStream = new ByteArrayOutputStream(symKey.getEncoded().length + iv.getIV().length);
+		//outputStream.write(symKey.getEncoded());
+		//outputStream.write(iv.getIV());
+
+		// Create socket
+		DatagramSocket socket = new DatagramSocket(clientPort);
+        // Create request message
+		JsonObject requestJson = JsonParser.parseString("{}").getAsJsonObject();
+		
+        JsonObject infoJson = JsonParser.parseString("{}").getAsJsonObject();
+        infoJson.addProperty("to", "BFTB");
+		infoJson.addProperty("from", accountPublicKey.getEncoded().toString());
+
+        //byte[] cipheredBody = symCipher.doFinal(bodyText.getBytes());
+        //String bodyEnc = Base64.getEncoder().encodeToString(cipheredBody);
+        infoJson.addProperty("body", bodyText);
+        infoJson.addProperty("instant", inst.toString());
+
+        requestJson.add("info", infoJson);
+
+        msgDig.update(infoJson.toString().getBytes());
+        String macString = Base64.getEncoder().encodeToString(signCipher.doFinal(msgDig.digest()));
+        requestJson.addProperty("MAC", macString);
+		
+		System.out.println("Request message: " + requestJson);
+		
+		// Send request
+		byte[] clientData = requestJson.toString().getBytes();
+		System.out.printf("%d bytes %n", clientData.length);
+		DatagramPacket clientPacket = new DatagramPacket(clientData, clientData.length, serverAddress, serverPort);
+		socket.send(clientPacket);
+		System.out.printf("Request packet sent to %s:%d!%n", serverAddress, serverPort);
+
+		// Receive response
+		byte[] serverData = new byte[BUFFER_SIZE];
+		DatagramPacket serverPacket = new DatagramPacket(serverData, serverData.length);
+		System.out.println("Wait for response packet...");
+		socket.receive(serverPacket);
+		System.out.printf("Received packet from %s:%d!%n", serverPacket.getAddress(), serverPacket.getPort());
+		System.out.printf("%d bytes %n", serverPacket.getLength());
+
+		inst = Instant.now();
+		//symCipher.init(Cipher.DECRYPT_MODE, symKey, iv);
+		encryptCipher.init(Cipher.DECRYPT_MODE, bankPublic);
+
+		// Convert response to string
+		String serverText = new String(serverPacket.getData(), 0, serverPacket.getLength());
+		System.out.println("Received response: " + serverText);
+
+		// Parse JSON and extract arguments
+		JsonObject responseJson = JsonParser.parseString(serverText).getAsJsonObject();
+        JsonObject infoBankJson;
+		String from, body, to, mac, instantBank;
+		
+        infoBankJson = responseJson.getAsJsonObject("info");
+        from = infoBankJson.get("from").getAsString();
+        to = infoBankJson.get("to").getAsString();
+        body = infoBankJson.get("body").getAsString();
+        instantBank = infoBankJson.get("instant").getAsString();
+        
+        mac = responseJson.get("MAC").getAsString();
+		
+        int messageCheck = checkMessage(encryptCipher, mac, msgDig, infoBankJson, instantBank, inst);
+		//String bodyDec = new String(symCipher.doFinal(Base64.getDecoder().decode(body)));
+		// Close socket
+		socket.close();
+		System.out.println("Socket closed");
+
+		if (messageCheck == CORRECT){
+			return body;
+		} else{
+			return "Failed";
+		}
+
 	}
 
 }
